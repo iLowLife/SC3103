c2f52d3f029b1caaae2840381a8f200226b4273f
iLowLife <39988468+iLowLife@users.noreply.github.com>
2023-09-05 15:26:27 +0800

    Update show.c

--- Hardware/show.c
+++ Hardware/show.c
@@ -10,7 +10,7 @@ void show_task(void *pvParameters)
    u32 lastWakeTime = getSysTickCnt();
 	
 	 OLED_Refresh_Gram();
-	 OLED_ShowString(0,0,"Low Cheng Feng");
+	 OLED_ShowString(0,0,"iLowLife");
    while(1)
     {	
 
@@ -29,14 +29,20 @@ void oled_show(void)
 
 		seconds++;
 	  if (seconds%60 == 0){
+			seconds = 0; //reset seconds
 			minutes++;
 		}
 		sprintf(string_buffer2 , "%d", minutes);
 		OLED_ShowString(10,20,"Minutes:");
 		OLED_ShowString(90,20,string_buffer2);
-		sprintf(string_buffer1 , "%d", seconds%60);
+		
+		if (seconds%60 < 10)
+			sprintf(string_buffer1 , "0%d", seconds%60);
+		else
+			sprintf(string_buffer1 , "%d", seconds%60);
 		OLED_ShowString(10,40,"Second:");
 		OLED_ShowString(90,40,string_buffer1);
+		
 		OLED_Refresh_Gram();
 		vTaskDelay(1000);
 		
